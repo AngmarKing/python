fc513b73e38b66455f9ecf49f9fd7670d2d3ff56
AngmarKing <75813374+AngmarKing@users.noreply.github.com>
2020-12-21 17:35:33 +0100

    Update chapter9_2.py

--- chapter9_2.py
+++ chapter9_2.py
@@ -1,23 +1,3 @@
-#def formel (x):
-#    wert = 2 * x * x + 4 * x + 9
-#    return wert
-#
-#ergebnis = formel(4)
-#print(ergebnis)
-
-#def formel (x):
-#    return 2 * x * x + 4 * x + 9
-#print (formel(4))
-
-#def formel (x):
-#    wert1 = 2 * x * x + 4 * x + 9
-#    wert2 = 3 * x * x + 2 * x - 7
-#    return [wert1, wert2]
-#
-#ergebnisse = formel (4)
-#print ("Ergebnis der ersten Formel:",ergebnisse[0])
-#print ("Ergebnis der zweiten Formel:",ergebnisse[1])
-
 def formel (x):
     if 2 * x * x + 4 * x + 9 > 50:
         return 2 * x * x + 4 * x + 9
